d73eb4d72ee550e4e2f7c532a995638d2c957e82
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-02-16 08:50:50 +0000

    Additional unit tests

--- test/controllers/actions/AffinityGroupIdentifierActionSpec.scala
+++ test/controllers/actions/AffinityGroupIdentifierActionSpec.scala
@@ -45,13 +45,18 @@ class AffinityGroupIdentifierActionSpec extends SpecBase {
     Future.successful(new ~(new ~(Some("id"), Some(affinityGroup)), enrolment))
 
   private val agentEnrolment = Enrolments(Set(Enrolment("HMRC-AS-AGENT", List(EnrolmentIdentifier("AgentReferenceNumber", "SomeVal")), "Activated", None)))
+  private val emptyAgentEnrolment = Enrolments(Set(Enrolment("HMRC-AS-AGENT", List(EnrolmentIdentifier("AgentReferenceNumber", "")), "Activated", None)))
   private val trustsTaxableEnrolment = Enrolments(Set(Enrolment("HMRC-TERS-ORG", List(EnrolmentIdentifier("SAUTR", utr)), "Activated", None)))
+  private val emptyTaxableEnrolment = Enrolments(Set(Enrolment("HMRC-TERS-ORG", List(EnrolmentIdentifier("SAUTR", "")), "Activated", None)))
   private val trustsNonTaxableEnrolment = Enrolments(Set(Enrolment("HMRC-TERSNT-ORG", List(EnrolmentIdentifier("URN", utr)), "Activated", None)))
+  private val emptyNonTaxableEnrolment = Enrolments(Set(Enrolment("HMRC-TERSNT-ORG", List(EnrolmentIdentifier("URN", "")), "Activated", None)))
 
   "invoking an AuthenticatedIdentifier" when {
-    "an Agent user" when {
-      "hasn't enrolled an Agent Services Account" must {
-        "redirect the user to the create agent services page" in {
+    "an Agent user" must {
+
+      "redirect the user to the create agent services page" when {
+
+        "hasn't enrolled an Agent Services Account" in {
 
           val application = applicationBuilder(userAnswers = None).build()
 
@@ -64,9 +69,24 @@ class AffinityGroupIdentifierActionSpec extends SpecBase {
           redirectLocation(result) mustBe Some(appConfig.createAgentServicesAccountUrl)
           application.stop()
         }
+
+        "has an empty agent enrolment" in {
+
+          val application = applicationBuilder(userAnswers = None).build()
+
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
+            .thenReturn(authRetrievals(AffinityGroup.Agent, emptyAgentEnrolment))
+
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(appConfig.createAgentServicesAccountUrl)
+          application.stop()
+        }
+
       }
 
-      "has correct enrolled in Agent Services Account" must {
+      "has correctly enrolled an Agent Services Account" must {
         "allow user to continue" in {
 
           val application = applicationBuilder(userAnswers = None).build()
@@ -83,6 +103,7 @@ class AffinityGroupIdentifierActionSpec extends SpecBase {
     }
 
     "an Org user" when {
+
       "with no enrolments" must {
         "allow user to continue" in {
 
@@ -97,164 +118,201 @@ class AffinityGroupIdentifierActionSpec extends SpecBase {
           application.stop()
         }
       }
-      "with taxable trusts enrolments" must {
-        "redirect to maintain-a-trust" in {
+
+      "with taxable trusts enrolments" when {
+
+        "enrolment has an identifier" must {
+          "redirect to maintain-a-trust" in {
+
+            val application = applicationBuilder(userAnswers = None).build()
+
+            when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
+              .thenReturn(authRetrievals(AffinityGroup.Organisation, trustsTaxableEnrolment))
+
+            val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+
+            status(result) mustBe SEE_OTHER
+            redirectLocation(result).value mustBe s"${appConfig.maintainATrustFrontendUrl}"
+            application.stop()
+          }
+        }
+
+        "enrolment has no identifier" must {
+          "continue without enrolment" in {
+
+            val application = applicationBuilder(userAnswers = None).build()
+
+            when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
+              .thenReturn(authRetrievals(AffinityGroup.Organisation, emptyTaxableEnrolment))
+
+            val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+
+            status(result) mustBe OK
+            application.stop()
+          }
+        }
+
+        "with non-taxable trusts enrolments" when {
+
+          "enrolment has an identifier" must {
+            "redirect to maintain-a-trust" in {
+
+              val application = applicationBuilder(userAnswers = None).build()
+
+              when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
+                .thenReturn(authRetrievals(AffinityGroup.Organisation, trustsNonTaxableEnrolment))
+
+              val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+
+              status(result) mustBe SEE_OTHER
+              redirectLocation(result).value mustBe s"${appConfig.maintainATrustFrontendUrl}"
+              application.stop()
+            }
+          }
+          "enrolment has no identifier" must {
+            "continue without enrolment" in {
+
+              val application = applicationBuilder(userAnswers = None).build()
+
+              when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
+                .thenReturn(authRetrievals(AffinityGroup.Organisation, emptyNonTaxableEnrolment))
+
+              val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+
+              status(result) mustBe OK
+              application.stop()
+            }
+          }
+        }
+      }
+
+      "an Individual user" must {
+        "redirect the user to the unauthorised page" in {
 
           val application = applicationBuilder(userAnswers = None).build()
 
           when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
-            .thenReturn(authRetrievals(AffinityGroup.Organisation, trustsTaxableEnrolment))
+            .thenReturn(authRetrievals(AffinityGroup.Individual, noEnrollment))
 
           val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
           status(result) mustBe SEE_OTHER
-          redirectLocation(result).value mustBe s"${appConfig.maintainATrustFrontendUrl}"
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+
           application.stop()
         }
       }
-      "with non-taxable trusts enrolments" must {
-        "redirect to maintain-a-trust" in {
+
+      "the user hasn't logged in" must {
+        "redirect the user to log in " in {
 
           val application = applicationBuilder(userAnswers = None).build()
 
-          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
-            .thenReturn(authRetrievals(AffinityGroup.Organisation, trustsNonTaxableEnrolment))
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed MissingBearerToken())
 
           val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
           status(result) mustBe SEE_OTHER
-          redirectLocation(result).value mustBe s"${appConfig.maintainATrustFrontendUrl}"
+
+          redirectLocation(result).get must startWith(frontendAppConfig.loginUrl)
           application.stop()
         }
       }
-    }
 
-    "and Individual user" must {
-      "redirect the user to the unauthorised page" in {
+      "the user's session has expired" must {
+        "redirect the user to log in " in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any()))
-          .thenReturn(authRetrievals(AffinityGroup.Individual, noEnrollment))
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed BearerTokenExpired())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          status(result) mustBe SEE_OTHER
 
-        application.stop()
+          redirectLocation(result).get must startWith(frontendAppConfig.loginUrl)
+          application.stop()
+        }
       }
-    }
 
-    "the user hasn't logged in" must {
-      "redirect the user to log in " in {
+      "the user doesn't have sufficient enrolments" must {
+        "redirect the user to the unauthorised page" in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed MissingBearerToken())
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed InsufficientEnrolments())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
+          status(result) mustBe SEE_OTHER
 
-        redirectLocation(result).get must startWith(frontendAppConfig.loginUrl)
-        application.stop()
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          application.stop()
+        }
       }
-    }
 
-    "the user's session has expired" must {
-      "redirect the user to log in " in {
+      "the user doesn't have sufficient confidence level" must {
+        "redirect the user to the unauthorised page" in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed BearerTokenExpired())
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed InsufficientConfidenceLevel())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
+          status(result) mustBe SEE_OTHER
 
-        redirectLocation(result).get must startWith(frontendAppConfig.loginUrl)
-        application.stop()
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          application.stop()
+        }
       }
-    }
 
-    "the user doesn't have sufficient enrolments" must {
-      "redirect the user to the unauthorised page" in {
+      "the user used an unaccepted auth provider" must {
+        "redirect the user to the unauthorised page" in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed InsufficientEnrolments())
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedAuthProvider())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
+          status(result) mustBe SEE_OTHER
 
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
-        application.stop()
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          application.stop()
+        }
       }
-    }
 
-    "the user doesn't have sufficient confidence level" must {
-      "redirect the user to the unauthorised page" in {
+      "the user has an unsupported affinity group" must {
+        "redirect the user to the unauthorised page" in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed InsufficientConfidenceLevel())
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedAffinityGroup())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
+          status(result) mustBe SEE_OTHER
 
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
-        application.stop()
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          application.stop()
+        }
       }
-    }
 
-    "the user used an unaccepted auth provider" must {
-      "redirect the user to the unauthorised page" in {
+      "the user has an unsupported credential role" must {
+        "redirect the user to the unauthorised page" in {
 
-        val application = applicationBuilder(userAnswers = None).build()
+          val application = applicationBuilder(userAnswers = None).build()
 
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedAuthProvider())
+          when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedCredentialRole())
 
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
+          val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
 
-        status(result) mustBe SEE_OTHER
+          status(result) mustBe SEE_OTHER
 
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
-        application.stop()
-      }
-    }
-
-    "the user has an unsupported affinity group" must {
-      "redirect the user to the unauthorised page" in {
-
-        val application = applicationBuilder(userAnswers = None).build()
-
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedAffinityGroup())
-
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
-
-        status(result) mustBe SEE_OTHER
-
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
-        application.stop()
-      }
-    }
-
-    "the user has an unsupported credential role" must {
-      "redirect the user to the unauthorised page" in {
-
-        val application = applicationBuilder(userAnswers = None).build()
-
-        when(mockAuthConnector.authorise(any(), any[Retrieval[RetrievalType]]())(any(), any())) thenReturn (Future failed UnsupportedCredentialRole())
-
-        val result = new AffinityGroupIdentifierAction(fakeAction, trustsAuth, appConfig).apply(fakeRequest)
-
-        status(result) mustBe SEE_OTHER
-
-        redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
-        application.stop()
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad().url)
+          application.stop()
+        }
       }
     }
   }
