e4f30d3b0b66052107cc25a90f281c921fb8f0eb
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-02-24 09:03:45 +0000

    Controller, view and page for does trst own or have controlling interest in non-eea company.

---
+++ app/controllers/asset/TrustOwnsNonEeaBusinessYesNoController.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.asset
+
+import controllers.actions.{DraftIdRetrievalActionProvider, RegistrationDataRequiredAction, RegistrationIdentifierAction}
+import forms.YesNoFormProvider
+import models.requests.RegistrationDataRequest
+import navigation.Navigator
+import pages.asset.TrustOwnsNonEeaBusinessYesNoPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, ActionBuilder, AnyContent, MessagesControllerComponents}
+import repositories.RegistrationsRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.asset.TrustOwnsNonEeaBusinessYesNoView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class TrustOwnsNonEeaBusinessYesNoController @Inject()(
+                                                        override val messagesApi: MessagesApi,
+                                                        repository: RegistrationsRepository,
+                                                        navigator: Navigator,
+                                                        identify: RegistrationIdentifierAction,
+                                                        getData: DraftIdRetrievalActionProvider,
+                                                        requireData: RegistrationDataRequiredAction,
+                                                        yesNoFormProvider: YesNoFormProvider,
+                                                        val controllerComponents: MessagesControllerComponents,
+                                                        view: TrustOwnsNonEeaBusinessYesNoView
+                                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  val form: Form[Boolean] = yesNoFormProvider.withPrefix("trustOwnsNonEeaBusinessYesNo")
+
+  private def actions(draftId: String): ActionBuilder[RegistrationDataRequest, AnyContent] =
+    identify andThen
+      getData(draftId) andThen
+      requireData
+
+  def onPageLoad(draftId: String): Action[AnyContent] = actions(draftId) {
+    implicit request =>
+
+      val preparedForm = request.userAnswers.get(TrustOwnsNonEeaBusinessYesNoPage) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, draftId))
+  }
+
+  def onSubmit(draftId: String): Action[AnyContent] = actions(draftId).async {
+    implicit request =>
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(view(formWithErrors, draftId))),
+
+        value => {
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(TrustOwnsNonEeaBusinessYesNoPage, value))
+            _              <- repository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(TrustOwnsNonEeaBusinessYesNoPage, draftId)(updatedAnswers))
+        }
+      )
+  }
+}
