6b3a6be61458958933de2f08246929a759efa311
carrielies <carrielies@users.noreply.github.com>
2021-05-27 11:26:58 +0100

    TRUS-4037 Allow removing of PropertyOrLand Asset

---
+++ app/controllers/asset/property_or_land/remove/RemoveAssetYesNoController.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.asset.property_or_land.remove
+
+import controllers.actions.StandardActionSets
+import forms.RemoveIndexFormProvider
+import handlers.ErrorHandler
+import javax.inject.Inject
+import models.RemoveAsset
+import models.assets.AssetNameType
+import play.api.Logging
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import services.TrustService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.asset.property_or_land.remove.RemoveAssetYesNoView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RemoveAssetYesNoController @Inject()(
+                                            override val messagesApi: MessagesApi,
+                                            standardActionSets: StandardActionSets,
+                                            formProvider: RemoveIndexFormProvider,
+                                            trustService: TrustService,
+                                            val controllerComponents: MessagesControllerComponents,
+                                            view: RemoveAssetYesNoView,
+                                            errorHandler: ErrorHandler
+                                          )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with Logging {
+
+  private val messagesPrefix: String = "propertyOrLand.removeYesNo"
+  private val form = formProvider.apply(messagesPrefix)
+
+  def onPageLoad(index: Int): Action[AnyContent] = standardActionSets.identifiedUserWithData.async {
+    implicit request =>
+
+      trustService.getPropertyOrLandAsset(request.userAnswers.identifier, index).map {
+        asset =>
+          Ok(view(form, index, asset.buildingLandName.getOrElse("")))
+      } recoverWith {
+        case iobe: IndexOutOfBoundsException =>
+          logger.warn(s"[Session ID: ${utils.Session.id(hc)}][UTR: ${request.userAnswers.identifier}]" +
+            s" user cannot remove asset as asset was not found ${iobe.getMessage}: IndexOutOfBoundsException")
+
+          Future.successful(Redirect(controllers.asset.nonTaxableToTaxable.routes.AddAssetsController.onPageLoad()))
+        case _ =>
+          logger.error(s"[Session ID: ${utils.Session.id(hc)}][UTR/URN: ${request.userAnswers.identifier}]" +
+            s" user cannot remove asset as asset was not found")
+          Future.successful(InternalServerError(errorHandler.internalServerErrorTemplate))
+      }
+  }
+
+  def onSubmit(index: Int): Action[AnyContent] = standardActionSets.identifiedUserWithData.async {
+    implicit request =>
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) => {
+          trustService.getNonEeaBusinessAsset(request.userAnswers.identifier, index).map {
+            asset =>
+              BadRequest(view(formWithErrors, index, asset.orgName))
+          }
+        },
+        value => {
+          if (value) {
+            trustService.getPropertyOrLandAsset(request.userAnswers.identifier, index).flatMap {
+              _ =>
+                trustService.removeAsset(request.userAnswers.identifier, RemoveAsset(AssetNameType.PropertyOrLandAssetNameType, index)).map(_ =>
+                  Redirect(controllers.asset.nonTaxableToTaxable.routes.AddAssetsController.onPageLoad())
+                )
+            }
+          } else {
+            Future.successful(Redirect(controllers.asset.nonTaxableToTaxable.routes.AddAssetsController.onPageLoad()))
+          }
+        }
+      )
+  }
+}
