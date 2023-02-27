80605cc2a1ad696d40ff1691ba23ef47a2174994
garytate123 <garytatelufc@gmail.com>
2023-02-27 12:00:52 +0000

    Addressed platops-pr-bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,19 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.14.0"
+
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.74.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "6.2.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.1.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.8.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "0.74.0",
