96745848acda1eeddbb6cd1b8e1465029b18c9cc
Ina Toni-Uebari <74660663+ina-tu@users.noreply.github.com>
2023-10-25 09:46:12 +0100

    DDCE-4714: removed unused configs + upgraded dependencies (#97)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.22.0"
   val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.23.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion
@@ -18,11 +18,11 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
-    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.16.0",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
+    "org.scalatest"               %% "scalatest"                % "3.2.17",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
+    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.17.0",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.2.0",
+    "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % "it, test")
 
