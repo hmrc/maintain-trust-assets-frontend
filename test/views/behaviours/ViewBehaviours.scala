967e0aee2a23eec967a09f22ba90fcd9b35639fc
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2024-06-18 09:27:10 +0100

    DDCE-5544 update to play-frontend-hmrc v9 (#103)

--- test/views/behaviours/ViewBehaviours.scala
+++ test/views/behaviours/ViewBehaviours.scala
@@ -22,6 +22,9 @@ import views.ViewSpecBase
 
 trait ViewBehaviours extends ViewSpecBase {
 
+  private def findBannerTitle(view:HtmlFormat.Appendable):String =
+    asDocument(view).getElementsByClass("govuk-header__link govuk-header__service-name").html()
+
   def normalPage(view: HtmlFormat.Appendable,
                  messageKeyPrefix: String,
                  ignoreTitle: Boolean = false): Unit = {
@@ -31,10 +34,7 @@ trait ViewBehaviours extends ViewSpecBase {
       "rendered" must {
 
         "have the correct banner title" in {
-
-          val doc = asDocument(view)
-          val bannerTitle = doc.getElementsByClass("hmrc-header__service-name hmrc-header__service-name--linked")
-          bannerTitle.html() mustBe messages("service.name")
+          findBannerTitle(view) mustBe messages("service.name")
         }
 
         "display the correct browser title" in {
@@ -90,10 +90,7 @@ trait ViewBehaviours extends ViewSpecBase {
       "rendered" must {
 
         "have the correct banner title" in {
-
-          val doc = asDocument(view)
-          val bannerTitle = doc.getElementsByClass("hmrc-header__service-name hmrc-header__service-name--linked")
-          bannerTitle.html() mustBe messages("service.name")
+          findBannerTitle(view) mustBe messages("service.name")
         }
 
         "display the correct browser title" in {
