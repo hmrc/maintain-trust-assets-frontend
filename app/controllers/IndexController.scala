19d23acdea6669d2a3f17201d25f5be3b9e89f19
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2025-06-10 11:48:27 +0100

    Partnership changes to avoid duplication of entries when going from check-answers page.

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -75,7 +75,7 @@ class IndexController @Inject()(
         } yield {
 
           println("request.userAnswers :::::::::::::::: "+assets +":::::::::::::: "+ assets.totalSizeCount)
-          Redirect(navigator.redirectToAddAssetPage(ua.isMigratingToTaxable, Some(assets.totalSizeCount)))
+          Redirect(navigator.redirectToAddAssetPage(ua.isMigratingToTaxable, Some(0)))
         }
     }
 
