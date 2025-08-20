ce14ffdf34ceeded90689ca2575249c3440fbdae
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2025-08-20 09:36:14 +0100

    Bootstrap version updated to 9.19.0 as per Victor's comment.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.13.0"
-  val mongoVersion = "2.6.0"
+  val bootstrapVersion = "9.19.0"
+  val mongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
