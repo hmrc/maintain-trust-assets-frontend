804ad4c70f0819e5f87a740bf8443f78d06e8786
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2026-09-22 14:23:45 +0100

    DDCE-9183 update mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
   val bootstrapVersion = "10.8.0"
-  val mongoVersion     = "2.13.0"
+  val mongoVersion     = "2.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
